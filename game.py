ac515a45d2bd9faf02975449cca5eee5b0d8f688
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2023-11-09 17:50:50 +0000

    add

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ class Game:
         self.moves = [None, None]
         self.wins = [0,0]
         self.ties = 0
+        self.payout_done = False
 
     def get_player_move(self, p):
         """
@@ -29,26 +30,45 @@ class Game:
         return self.p1Went and self.p2Went
 
     def winner(self):
+        if self.payout_done:
+            return -1, None, None, None  # return a placeholder value
 
         p1 = self.moves[0].upper()[0]
         p2 = self.moves[1].upper()[0]
 
         winner = -1
+        # add winning conditions, grab winning address, post to blockchain
         if p1 == "R" and p2 == "S":
             winner = 0
+            winner_uid = user1_uid
+            loser_uid = user2_uid
         elif p1 == "S" and p2 == "R":
             winner = 1
+            winner_uid = user2_uid
+            loser_uid = user1_uid
         elif p1 == "P" and p2 == "R":
             winner = 0
+            winner_uid = user1_uid
+            loser_uid = user2_uid
         elif p1 == "R" and p2 == "P":
             winner = 1
+            winner_uid = user2_uid
+            loser_uid = user1_uid
         elif p1 == "S" and p2 == "P":
             winner = 0
+            winner_uid = user1_uid
+            loser_uid = user2_uid
         elif p1 == "P" and p2 == "S":
             winner = 1
-
-        return winner
+            winner_uid = user2_uid
+            loser_uid = user1_uid
+        note = "Player 1 chose {0} and player 2 chose {1}.".format(p1,p2)
+        return winner, winner_uid, loser_uid, note
 
     def resetWent(self):
         self.p1Went = False
         self.p2Went = False
+
+# define below user ids based on player 1 or player 2
+user1_uid = "CEGWAX2saBV3bh0sNt5Q0yUxYH13" # test user uid 1 = player 1 
+user2_uid = "4vqh7TmHNQSMxRkuzUuktNlHjeZ2" # test user uid 2 = player 2
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 from _thread import *
 import pickle
 from game import Game
+import threading
 
 server = "localhost"
 port = 5555
@@ -20,6 +21,8 @@ connected = set()
 games = {}
 idCount = 0
 
+# Add this global variable at the beginning of your script
+executed_payouts = {}
 
 def threaded_client(conn, p, gameId):
     global idCount
@@ -41,6 +44,21 @@ def threaded_client(conn, p, gameId):
                     elif data != "get":
                         game.play(p, data)
 
+                    # start server change
+                    if game.bothWent():
+                        winner, winner_uid, loser_uid, note = game.winner()
+                        if winner != -1 and not game.payout_done:
+                            lock = threading.Lock()
+                            lock.acquire()
+                            try:
+                                if gameId not in executed_payouts:
+                                    payout_winner(loser_uid, winner_uid, note)
+                                    executed_payouts[gameId] = True  # mark that payout has been done for this game
+                            finally:
+                                lock.release()
+                    # end server change
+
+
                     conn.sendall(pickle.dumps(game))
             else:
                 break
@@ -50,12 +68,43 @@ def threaded_client(conn, p, gameId):
     print("Lost connection")
     try:
         del games[gameId]
+        if gameId in executed_payouts:  # remove the game id from executed_payouts when game is closed
+            del executed_payouts[gameId]
         print("Closing Game", gameId)
     except:
         pass
-    idCount -= 1
-    conn.close()
 
+# [ Start DropChain API Integrations ]
+def payout_winner(loser_uid, winner_uid, note): 
+    # get the game instance
+    game = games.get(gameId, None)
+    if game is None or game.payout_done:
+        return  # exit the function if the game does not exist or the payout has already been done
+    
+    game.payout_done = True  # set the payout_done flag to True
+    
+    import requests, json 
+    app_id = "BNCYKR4B39XimiWcS4UvgeGu" # app_id 
+
+    url = "https://api.dropchain.network/v1/send_algo_testnet"
+
+    payload = {
+    "app_id": app_id,
+        "user1_uid": loser_uid,
+        "receiver1_uid": winner_uid,
+        "asset1_amount_int": "100000", # this amount can be changed based on the wager for the game
+        "transaction1_note": note
+    }
+    headers = {
+        "content-type": "application/json",
+        "X-API-Key": "7096410e-6d70-4846-9082-b0a242f29b1b", # taken from your DropChain API dashboard
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    response = json.loads(response.text)
+    print(response)
+    return response
+# [ End DropChain API Integrations ]
 
 
 while True:
